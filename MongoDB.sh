76490ae4656a5dfb15b144c920b33e03e290aeb6
msaivenkatasiva <msaivenkatasiva@gmal.com>
2025-03-01 23:15:56 +0530

    Roboshop

--- MongoDB.sh
+++ MongoDB.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 ID=$(id -u)
-timestamp=$(time +%F-%H-%M-%S)
+timestamp=$(date +%F-%H-%M-%S)
 logfile="/tmp/$0-$timestamp.log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+
 
 validate(){
 if [ $1 -ne 0 ]
@@ -24,10 +25,11 @@ else
 echo "your root user"
 fi
 
-for $package in $@
+for package in $@
 do
 yum list installed $package  &>>$logfile
 validate $? "$package is not installed"
 yum install $package -y
 validate $? "$package installed"
 done
+
